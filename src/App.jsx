73304623263fc53ea7d9ba31bd3363ec0b08e565
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2023-03-05 09:43:13 +0200

    project refined changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import reactLogo from "./assets/react.svg";
+// import "./index.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hey</h1>
+    </div>
+  );
+};
+
+export default App;
